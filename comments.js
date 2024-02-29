c5d8a03cc66abd78edd12b4c930ef2f87e8bd6eb
GivenCloud <129170325+GivenCloud@users.noreply.github.com>
2024-02-29 09:05:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,56 +1,80 @@
-//Create a new server side route to handle the new comment form submission.
-//This route will be responsible for adding a new comment to the database.
-//The form data will be sent as a POST request to the server.
-//The server will then add the new comment to the database and return the updated list of comments.
-//The updated list of comments will be sent back to the client and displayed on the page.
-
+//Create a new server side route for comments
+// Create a new express router
 const express = require('express');
 const router = express.Router();
+// Import the Comment model
 const Comment = require('../models/comment');
-const Post = require('../models/post');
+// Import the authentication middleware
+const passport = require('passport');
+// Import the User model
 const User = require('../models/user');
-const { body, validationResult } = require('express-validator');
+// Import the Post model
+const Post = require('../models/post');
 
-// GET request to display the new comment form
-router.get('/new', (req, res) => {
-  res.render('comments/new');
+// POST /comments
+// Purpose: Create a new comment
+// Request: JSON object with the comment content and the ID of the post
+// Response: JSON object with the new comment
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // Create a new comment
+    const newComment = new Comment({
+        content: req.body.content,
+        user: req.user.id,
+        post: req.body.post
+    });
+    // Save the comment to the database
+    newComment.save().then(comment => {
+        // Send the new comment as a JSON object
+        res.json(comment);
+    }).catch(err => {
+        // If there was an error saving the comment, send a 400 response with the error
+        res.status(400).json(err);
+    });
 });
 
-// POST request to handle the new comment form submission
-router.post(
-  '/',
-  // Validate and sanitize the fields
-  body('text')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('Comment must not be empty')
-    .escape(),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.render('comments/new', { errors: errors.array() });
-    } else {
-      try {
-        // Create a new comment
-        const comment = new Comment({
-          text: req.body.text,
-          timestamp: new Date(),
-          user: req.user._id,
-        });
-        // Save the new comment to the database
-        await comment.save();
-        // Add the new comment to the post
-        const post = await Post.findById(req.body.postId);
-        post.comments.push(comment);
-        await post.save();
-        // Redirect to the post detail page
-        res.redirect(`/posts/${req.body.postId}`);
-      } catch (err) {
-        console.error(err);
-        res.send('Error adding comment');
-      }
-    }
-  }
-);
+// GET /comments/:id
+// Purpose: Get all comments for a post
+// Request: ID of the post
+// Response: JSON object with all the comments for the post
+router.get('/:id', (req, res) => {
+    // Find all comments for the post
+    Comment.find({ post: req.params.id }).populate('user').then(comments => {
+        // Send the comments as a JSON object
+        res.json(comments);
+    }).catch(err => {
+        // If there was an error finding the comments, send a 400 response with the error
+        res.status(400).json(err);
+    });
+});
 
+// DELETE /comments/:id
+// Purpose: Delete a comment
+// Request: ID of the comment
+// Response: JSON object with the deleted comment
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // Find the comment by ID
+    Comment.findById(req.params.id).then(comment => {
+        // If the comment does not exist, send a 404 response
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+        // If the comment was created by a different user, send a 403 response
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'Not authorized' });
+        }
+        // Delete the comment
+        comment.remove().then(() => {
+            // Send the deleted comment as a JSON object
+            res.json(comment);
+        }).catch(err => {
+            // If there was an error deleting the comment, send a 400 response with the error
+            res.status(400).json(err);
+        });
+    }).catch(err => {
+        // If there was an error finding the comment, send a 400 response with the error
+        res.status(400).json(err);
+    });
+});
+
+// Export the router
 module.exports = router;
